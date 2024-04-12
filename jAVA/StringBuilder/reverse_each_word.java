9b270ebf429d901a6522a7388c5f747508c28a33
Najma099 <najmakhatun0999@gmail.com>
2024-04-12 22:41:52 +0530

    reverse_StringBuilder

--- jAVA/StringBuilder/reverse_each_word.java
+++ jAVA/StringBuilder/reverse_each_word.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
+
 public class reverse_each_word {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        StringBuilder S=new StringBuilder(sc.next());
-        n=S.length();
-        int i=0;
-        int j=0;
-        while(j<n)
-        {
-            if(S.charAt(j)!=" ")
-            j++;
-            else{
-                reverse(S,i,j-1);
-                i=j+1;
-                j=i;
+        Scanner sc = new Scanner(System.in);
+        StringBuilder S = new StringBuilder(sc.nextLine());
+        int n = S.length();
+        int i = 0;
+        int j = 0;
+        while (j < n) {
+            if (S.charAt(j) != ' ') {
+                j++;
+            } else {
+                reverse(S, i, j - 1);
+                i = j + 1;
+                j = i;
             }
         }
+        reverse(S, i, j - 1);
         System.out.println(S);
     }
-    public static void reverse(StringBuilder S,int i,int j )
-    {
-        while(i<=j)
-        {
-            char temp =S.charAt(i);
-            S.setCharAt(i,S.charAt(j));
-            S.setCharAt(j,temp);
+
+    public static void reverse(StringBuilder S, int i, int j) {
+        while (i < j) {
+            char temp = S.charAt(i);
+            S.setCharAt(i, S.charAt(j));
+            S.setCharAt(j, temp);
             i++;
             j--;
         }
