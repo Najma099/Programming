755e102853e6e563bfc47547c05f7660a0fe9f96
Najma099 <najmakhatun0999@gmail.com>
2024-05-01 09:47:46 +0530

    bit set using right shift

---
+++ jAVA/BITS/rightset.java
@@ -0,0 +1,11 @@
+public class rightset {
+    //if bit is set or not using right shift
+    public static void main(String[] args) {
+        int n=13;
+        int k=2;
+        if(((n>>k)&1)==1)
+        System.out.print("Set");
+        else
+        System.out.print("Not set");
+    }
+}
