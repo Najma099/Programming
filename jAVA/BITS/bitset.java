9aea1e93f908a2f5d714002fec76e75fc2a71a8b
Najma099 <najmakhatun0999@gmail.com>
2024-05-01 09:11:50 +0530

    bit is set or not

---
+++ jAVA/BITS/bitset.java
@@ -0,0 +1,10 @@
+public class bitset {
+    public static void main(String[] args) {
+        int N=13;
+        int k=2;
+        if((N &(1<<k))!=0)
+        System.out.println("Set");
+        else
+        System.out.println("Not set");
+    }
+}
---
+++ jAVA/BITS/swap.java
@@ -0,0 +1,11 @@
+public class swap {
+    public static void main(String[] args)
+    {
+        int a=7;
+        int b=6;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println(a);
+    }
+}
