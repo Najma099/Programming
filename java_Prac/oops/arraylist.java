2f85c093bb9a2e2a030fa78d58b49aff44eed647
Najma099 <najmakhatun0999@gmail.com>
2024-05-11 22:13:52 +0530

    arraylist implementation

--- java_Prac/oops/arraylist.java
+++ java_Prac/oops/arraylist.java
@@ -1,3 +1,33 @@
 public class arraylist {
-    
+    public static class ArrayListImpl {
+        static int[] arr = new int[6]; 
+        static int size = 0; 
+
+        public void add(int ele) { 
+            arr[size] = ele;
+            size++;
+        }
+        
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            for (int i = 0; i < size; i++) {
+                sb.append(arr[i]);
+                if (i < size - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayListImpl arr = new ArrayListImpl();
+        arr.add(2);
+        arr.add(7);
+        System.out.println(arr);
+    }
 }
