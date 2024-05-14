16c44a496471606385efc3f5a7288e5543953e2c
Najma099 <najmakhatun0999@gmail.com>
2024-05-14 23:05:35 +0530

    Linklist_implementation_addition

--- java_Prac/linklist.java/implementation.java
+++ java_Prac/linklist.java/implementation.java
@@ -12,6 +12,29 @@ public class implementation {
     {
         Node head=null;
         Node tail=null;
+        void insert(int index,int val)
+        {
+            Node newNode = new Node(val);
+            Node temp=head;
+            for(int i=1;i<=index-1;i++)
+            temp=temp.next;
+            newNode.next=temp.next;
+            temp.next=newNode;
+            
+        }
+        void insertAtBegin(int val)
+        {
+            Node newNode = new Node(val);
+            if(head==null)
+            {
+                insertAtEnd(val);
+            }
+            else
+            {
+                newNode.next=head;
+                head=newNode;
+            }
+        }
         void insertAtEnd(int val)
         {
             Node newNode = new Node(val);
@@ -54,5 +77,10 @@ public class implementation {
         ll.display();
         System.out.println();
         System.out.println(ll.length());
+        ll.insertAtBegin(6);
+        ll.display();
+        System.out.println();
+        ll.insert(1,77);
+        ll.display();
     }
 }
